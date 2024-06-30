2d4fa4a38bf004a43d7a5214006f40381dbcb7cc
Nasseima <lindsaynasseima@gmail.com>
2024-06-29 23:49:03 -0400

    All done!!

--- src/Question5.java
+++ src/Question5.java
@@ -1,2 +1,10 @@
 public class Question5 {
+    public static void main (String [] args) {
+
+        int x = 5;
+        System.out.println("The value of x is " + x);
+
+        x++;
+        System.out.println("Using a postfix increment operator the value of x is " + x);
+    }
 }
---
+++ src/Question6.java
@@ -0,0 +1,17 @@
+public class Question6 {
+    public static void main (String [] args) {
+        int x = 10;
+        System.out.println("The value of x is " + x);
+
+        System.out.println("Various ways to increment x by 1: ");
+
+        x++;
+        System.out.println(x);
+
+        ++x;
+        System.out.println(x);
+
+        x+=1;
+        System.out.println(x);
+    }
+}
